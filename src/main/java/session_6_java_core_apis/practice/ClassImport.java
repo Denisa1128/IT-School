8b9c9aea813f2002b1c4ae13c31ee6baec89a404
Denisa-Maria <denisaciocani1128@gmail.com>
2024-06-20 21:20:32 +0300

    session 6

---
+++ src/main/java/session_6_java_core_apis/practice/ClassImport.java
@@ -0,0 +1,15 @@
+package session_6_java_core_apis.practice;
+
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassImport {
+    public static void main(String[] args) {
+        String name ="Alice";
+
+        List<String> myList = new ArrayList<>();
+
+    }
+}
---
+++ src/main/java/session_6_java_core_apis/practice/StringBuilderPractice.java
@@ -0,0 +1,5 @@
+package session_6_java_core_apis.practice;
+
+public class StringBuilderPractice {
+
+}
