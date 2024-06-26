a138a1285823679c35b31076036449e854e849a7
Denisa-Maria <denisaciocani1128@gmail.com>
2024-06-26 23:07:21 +0300

    session 7

---
+++ src/main/java/session_7_utility_clases/practice/ObjectPractice.java
@@ -0,0 +1,17 @@
+package session_7_utility_clases.practice;
+
+
+    public class ObjectPractice {
+
+        public static void main(String[] args) {
+            Object object = new Object();
+
+            Car car = new Car();
+            Object secondObject = car;
+        }
+    }
+
+    class Car {
+
+    }
+
