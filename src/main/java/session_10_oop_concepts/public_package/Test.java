4217605eb7896da1794fc9864412187af4f8c677
Denisa-Maria <denisaciocani1128@gmail.com>
2024-07-07 08:37:22 +0300

    challenge session 10

---
+++ src/main/java/session_10_oop_concepts/public_package/Test.java
@@ -0,0 +1,22 @@
+package session_10_oop_concepts.public_package;
+
+import session_10_oop_concepts.sensitive_logic_package.Animal;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+       // System.out.println("Age:" +dog.age);
+        //System.out.println("Name:" +dog.name);
+
+       // System.out.println(dog.getName());
+       // System.out.println(dog.bark() );
+        String dogBark =
+
+    }
+    public int sum( int number1, int number2) {
+        return number1 + number2;
+
+    }
+
+}
---
+++ src/main/java/session_10_oop_concepts/sensitive_logic_package/Animal.java
@@ -0,0 +1,19 @@
+package session_10_oop_concepts.sensitive_logic_package;
+
+public class Animal {
+    // attributes
+    String name;
+    double weight;
+    int age;
+    String colour;
+    String breath;
+
+    String getName() {
+        return "Rex";
+
+    }
+    String bark (){
+        return " ham!";
+    }
+
+}
